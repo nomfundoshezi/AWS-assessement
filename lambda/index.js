70af0e7a52182d055a5027c278f7a703b542286d
Nomfundo Shezi <nomfundoshezi94@gmail.com>
2022-05-26 19:17:21 +0200

    Add s3 bucket name

--- lambda/index.js
+++ lambda/index.js
@@ -1,29 +1,29 @@
 var AWS = require('aws-sdk');
 
 exports.handler = async (event) => {
-    
+
     let S3 = new AWS.S3({ region: process.env.AWS_REGION });
-    
-    var email = event['rawPath'].replace('/','');
-    
-     var params = {
-         Bucket: 'test', //s3 bucket name to store emails
-         Key: email+".txt",
-         Body: JSON.stringify(email),
-         ContentType: 'text/plain',
+
+    var email = event['rawPath'].replace('/', '');
+
+    var params = {
+        Bucket: 'nomfundo-emails', //bucket to store email addresses
+        Key: email + ".txt",
+        Body: JSON.stringify(email),
+        ContentType: 'text/plain',
     };
-    
+
     let s3Response = await S3.upload(params).promise();
-    
+
     const response = {
-            'statusCode': 200,
-            'headers': { 'Content-Type': 'application/json' },
-            'body': JSON.stringify({
-                "email":email,
-                "s3Path":s3Response.Location
-            })
-        }
-    
+        'statusCode': 200,
+        'headers': { 'Content-Type': 'application/json' },
+        'body': JSON.stringify({
+            "email": email,
+            "s3Path": s3Response.Location
+        })
+    }
+
     return response;
-   
+
 };
